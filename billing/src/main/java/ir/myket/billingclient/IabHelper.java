afb8558d3f70fa037e6fbfdde7893d80ba2d21e5
komeil <kamal.komeil@gmail.com>
2025-02-17 17:56:15 +0330

    Use subscriptionsSupported() to check subscription support in queryInventory

--- billing/src/main/java/ir/myket/billingclient/IabHelper.java
+++ billing/src/main/java/ir/myket/billingclient/IabHelper.java
@@ -456,7 +456,7 @@ public class IabHelper {
                 }
             }
 
-            if (iabConnection.mSubscriptionsSupported) {
+            if (subscriptionsSupported()) {
                 r = queryPurchases(inv, ITEM_TYPE_SUBS);
                 if (r != BILLING_RESPONSE_RESULT_OK) {
                     throw new IabException(r, "Error refreshing inventory (querying owned subscriptions).");
