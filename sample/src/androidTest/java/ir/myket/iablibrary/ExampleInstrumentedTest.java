96a3b93ce925360951d8bd1233ad1e85a981b196
Reza Torabi <136625739+rezatutor475@users.noreply.github.com>
2025-04-10 21:11:24 +0330

    Update ExampleInstrumentedTest.java
    Improved

--- sample/src/androidTest/java/ir/myket/iablibrary/ExampleInstrumentedTest.java
+++ sample/src/androidTest/java/ir/myket/iablibrary/ExampleInstrumentedTest.java
@@ -1,8 +1,13 @@
-package ir.myket.iablibrary;
+package ir.myket.billingclient;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -17,10 +22,46 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ir.myket.iablibrary", appContext.getPackageName());
+        assertEquals("ir.myket.billingclient", appContext.getPackageName());
+    }
+
+    @Test
+    public void appVersionCodeIsPositive() throws Exception {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        PackageManager pm = appContext.getPackageManager();
+        PackageInfo info = pm.getPackageInfo(appContext.getPackageName(), 0);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            assertTrue("Version code must be positive", info.getLongVersionCode() > 0);
+        } else {
+            assertTrue("Version code must be positive", info.versionCode > 0);
+        }
+    }
+
+    @Test
+    public void appVersionNameIsNotEmpty() throws Exception {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        PackageManager pm = appContext.getPackageManager();
+        PackageInfo info = pm.getPackageInfo(appContext.getPackageName(), 0);
+
+        assertNotNull("Version name should not be null", info.versionName);
+        assertTrue("Version name should not be empty", info.versionName.trim().length() > 0);
+    }
+
+    @Test
+    public void appHasPackageManager() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        PackageManager pm = appContext.getPackageManager();
+        assertNotNull("Package manager should be available", pm);
+    }
+
+    @Test
+    public void sdkVersionIsValid() {
+        int sdkVersion = Build.VERSION.SDK_INT;
+        assertTrue("SDK version must be greater than zero", sdkVersion > 0);
     }
 }
